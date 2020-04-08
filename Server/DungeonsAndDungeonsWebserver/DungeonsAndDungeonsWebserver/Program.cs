2b4b34ef564ae913eed854e5107e8fa66defd2b3
mineorbit <maximilian.stillger@arcor.de>
2020-04-08 19:05:10 +0200

    Grundlegende ServerProjektdateien

---
+++ Server/DungeonsAndDungeonsWebserver/DungeonsAndDungeonsWebserver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DungeonsAndDungeonsWebserver
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
