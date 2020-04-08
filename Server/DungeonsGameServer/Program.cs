269ab48b0b057f4c2eb383982a53515e69c6779a
mineorbit <maximilian.stillger@arcor.de>
2020-04-08 19:15:06 +0200

    Server setup

---
+++ Server/DungeonsGameServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DungeonsGameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
