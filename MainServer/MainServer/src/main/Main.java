1dc84b4e1fbf8d084c4fe2fa00d07009d0244077
mineorbit <maximilian.stillger@arcor.de>
2020-04-09 23:19:32 +0200

    ja ez

--- MainServer/MainServer/src/main/Main.java
+++ MainServer/MainServer/src/main/Main.java
@@ -1,5 +1,12 @@
 package main;
 
 public class Main {
-
+	public Main()
+	{
+		
+	}
+	public static void main(String[] args)
+	{
+		new Main();
+	}
 }
---
+++ MainServer/MainServer/src/net/ClientHandler.java
@@ -0,0 +1,7 @@
+package net;
+
+public class ClientHandler {
+
+	
+	
+}
