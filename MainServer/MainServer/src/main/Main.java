c664ec985b4988e5f4688f954b557f561c67eb36
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-04-10 19:41:40 +0200

    refactor main

--- MainServer/MainServer/src/main/Main.java
+++ MainServer/MainServer/src/main/Main.java
@@ -3,14 +3,10 @@ package main;
 import java.io.IOException;
 
 public class Main {
-	public Main() throws IOException {
-		Server s = new Server(13565, 5);
-		s.start();
-	}
-
 	public static void main(String[] args) {
 		try {
-			new Main();
+			Server s = new Server(13565, 5);
+			s.start();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
