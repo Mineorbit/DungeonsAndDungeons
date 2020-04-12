2b2d10c9fa946b4fe0d90b69e9495d717814c834
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-04-12 19:34:32 +0200

    wip

--- MainServer/MainServer/src/util/Util.java
+++ MainServer/MainServer/src/util/Util.java
@@ -15,6 +15,10 @@ public class Util {
 	public static int readTwoBytes(byte a, byte b) {
 		return (int) a << 8 | (int) b;
 	}
+	
+	public static byte[] writeTwoBytes(int x) {
+		return new byte[] { (byte) (x >> 8), (byte) x };
+	}
 
 	public static byte[] subArray(byte[] data, int beg, int end) {
 		return Arrays.copyOfRange(data, beg, end + 1);
@@ -23,6 +27,9 @@ public class Util {
 	public static byte[] stringToBytes(String str) {
 		final byte[] raw = str.getBytes(StandardCharsets.UTF_8);
 		byte[] result = new byte[raw.length + 2];
+		byte[] len = writeTwoBytes(raw.length);
+		result[0] = len[0];
+		result[1] = len[1];
 		
 		for (int i = 0; i < raw.length; i++) {
 			result[i + 2] = raw[i];
@@ -32,10 +39,7 @@ public class Util {
 	}
 	
 	public static String bytesToString(byte[] bytes) {
-		int length = 0;
-		
-		length |= bytes[0];
-		length |= (int) bytes[1] << 8;
+		int length = readTwoBytes(bytes[0], bytes[1]);
 		
 		return new String(Arrays.copyOfRange(bytes, 2, 2 +  length), StandardCharsets.UTF_8);
 	}
