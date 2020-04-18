56575dba470b7b662c67b1e64bfa025b03c12ca4
mineorbit <maximilian.stillger@arcor.de>
2020-04-18 22:20:10 +0200

    Lol

--- DungeonServer/Assets/Scripts/Logic/ServerManager.cs
+++ DungeonServer/Assets/Scripts/Logic/ServerManager.cs
@@ -7,7 +7,7 @@ public class ServerManager : MonoBehaviour
 {
     public static ServerManager instance;
     public bool Local = true;
-    public enum State{Setup,Idle,Preparing,Ready,Playing,Won};
+    public enum State{Setup,Idle,Prepare,Play,GameOver};
     public static State s;
     public Player[] players;
 
@@ -20,22 +20,64 @@ public class ServerManager : MonoBehaviour
         {
             Destroy(this);
         }
-        SetupServer();
-        s = State.Setup;
 
+
+        Setup();
+
+       
+    }
+
+   
+
+    void Idle()
+    {
+        s = State.Idle;
+        //Do house keeping tasks like preloading favorite levels, manage memory etc
+
+
+    }
+    void Play()
+    {
+        s = State.Play;
+        //Block entries communicate to  lobby everything necessary
+    }
+
+
+    void  GameOver()
+    {
+        s = State.GameOver;
+        //Redirect Players and go back to  start
+
+    }
+
+    void Prepare()
+    {
+        s = State.Prepare;
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 64;
-
+        //Download and load map
         Debug.Log("Server wird gestartet");
         Server.Start(45565);
     }
+
+
     private void OnApplicationQuit()
     {
         Server.Stop();
+        //Deallocate everything / delete garbage / create logs
     }
-    void SetupServer()
+    void Setup()
     {
+
+        s = State.Setup;
+
         Directory.CreateDirectory(Application.persistentDataPath+"/map");
+
+        if(Local) 
+        {
+        GameLogic.current.playerCount = 1;
+        }
+        Prepare();
     }
 
     
