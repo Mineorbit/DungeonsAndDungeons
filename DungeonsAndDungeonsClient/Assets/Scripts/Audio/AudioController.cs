03fd8e344d77e5c9dd1045c4b1f6d2db26654a19
mineorbit <mineorbitbeta@gmail.com>
2020-09-24 13:36:00 +0200

    AudioManager erweitert

--- DungeonsAndDungeonsClient/Assets/Scripts/Audio/AudioController.cs
+++ DungeonsAndDungeonsClient/Assets/Scripts/Audio/AudioController.cs
@@ -7,16 +7,19 @@ public class AudioController : MonoBehaviour
     [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/AudioProfile", order = 1)]
     class AudioProfile : ScriptableObject
     {
-        public Vector3 relativePosition;
-        public float minVolume = 0.5f;
-        public float maxVolume = 1;
-        public float spatialFactor;
-        public bool loop;
-        public AudioClip audioClip;
+        public bool         overloading;
+        public Vector3      relativePosition;
+        public float        minVolume = 0.5f;
+        public float        maxVolume = 1;
+        public float        spatialFactor;
+        public bool         loop;
+        public AudioClip[]    audioClip;
     }
+    
 
     AudioProfile[] audioProfiles;
-    AudioSource[] audioSources;
+    AudioSource[][] audioSources;
+    int[] currentPlay;
     void Awake()
     {
         audioSources = new AudioSource[audioProfiles.Length];
@@ -25,11 +28,21 @@ public class AudioController : MonoBehaviour
 
     void SetupAudioProfiles()
     {
+        int j = 0;
+        currentPlay = new int[audioProfiles.Length];
         foreach(AudioProfile ap in audioProfiles)
         {
-            AudioSource source = transform.gameObject.AddComponent<AudioSource>();
-            source.loop = ap.loop;
-            source.clip = ap.audioClip;
+            AudioSource[] source = new AudioSource[ap.audioClip.Length];
+            int i = 0;
+            foreach (AudioClip c in ap.audioClip)
+            {
+                source[i] = transform.gameObject.AddComponent<AudioSource>();
+                source[i].loop = ap.loop;
+                source[i].clip = ap.audioClip;
+                i++;
+            }
+            audioSources[j] = source;
+            j++;
         }
     }
     public void Blend(int index, float t)
@@ -38,7 +51,31 @@ public class AudioController : MonoBehaviour
     }
     public void Play(int index)
     {
-        audioSources[index].Play();
+        if(!audioSources[index].isPlaying)
+        {
+            audioSources[index].Play();
+        }
+    }
+    public void PlayNext(int index)
+    {
+        int nextPlay = (currentPlay[index] + 1) % (audioSources[index].Length);
+        if (!audioSources[index][currentPlay[index]].isPlaying)
+        {
+
+            audioSources[index][currentPlay[index]].Stop();
+            audioSources[index][nextPlay].Play();
+        }
+
+        if (audioSources[index][currentPlay].isPlaying)
+        {
+
+            if (audioProfiles[index].overloading)
+            {
+                audioSources[index][nextPlay].Play();
+            }
+
+        }
+        currentPlay[index] = nextPlay;
     }
 
     public void Stop(int index)
