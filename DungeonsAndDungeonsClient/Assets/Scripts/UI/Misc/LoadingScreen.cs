c5ea7db844de81b11d318e9b01b3ee22e82be4ec
mineorbit <maximilian.stillger@arcor.de>
2021-01-12 21:56:07 +0100

    Fixed small errors

--- DungeonsAndDungeonsClient/Assets/Scripts/UI/Misc/LoadingScreen.cs
+++ DungeonsAndDungeonsClient/Assets/Scripts/UI/Misc/LoadingScreen.cs
@@ -12,17 +12,18 @@ public class LoadingScreen : Openable
 {
     UIAnimation animationScreen;
     UIAnimation animationInfoText;
+
     //transform of the info text
     public Transform text;
     public TextMeshProUGUI infoTextField;
     public static LoadingScreen instance;
     public UnityEvent openEvent;
+
     //Helps with blocking UI
     GraphicRaycaster rc;
     string[] infoText = { "Benutze W/A/S/D zum laufen",
                           "Manche Dungeons sind nur zu viert schaffbar",
                           "Wenn du mal nicht weiter kommst, kannst du jeder Zeit ohne Strafe die Runde abbrechen",
-                          "Der Plural von Wischmops ist Wischmöpse",
                           "Vielleicht gewinnst du etwas wenn du die Bestzeit schlägst",
                           "Wenn du einen Gegner schlägst, der die gleiche Farbe wie du hat, verursachst du mehr Schaden",
                           "Eier und Salz, Milch und Mehl",
@@ -34,13 +35,34 @@ public class LoadingScreen : Openable
                           "So ein geiles Produkt",
                           "Wer das liest, niest",
                           "Wer anderen eine Bratwurst brät, der hat ein Bratwurstbratgerät",
-                          "Immer wenn ich traurig bin, trink ich einen ...",
+                          "Immer wenn ich traurig bin, trink ich einen Korn",
                           "Ba-Ba-Banküberfall",
+                          "Wie eine Fatamorgana",
+                          "Mister Meier bitte sei mein Samurai",
                           "Für Garderobe keine Haftung",
                           "Gut gebügelt ist halb genäht",
+                          "Die A, B, C und die 6",
+                          "Haben wir noch Pepsi? Garkeine mehr?",
+                          "Wer ein Smiley zu viel macht, der hat irgendwann auch nixmehr zu lachen",
+                          "Ich bin der Held der Steine, in Frankfurt am Main, im Herzen von Europa, in meinem wunderschönen kleinen Lädchen," +
+                          "an einem fantastischen Tag und schaut mal, was ich euch mitgebracht habe :)",
+                          "Die Abenteuer der Rückseite",
+                          "Na Hallo wie gehts denn so",
+                          "General Genobi, euer Wagemut ist beeindruckend",
+                          "Häuser können nicht brennen",
+                          "Leute haut ma einen raus",
+                          "Die Sonne scheint, das Bier ist kalt",
+                          "Qualle",
+                          "100% futureknick frei",
+                          "anatomisch komisch",
+                          "Rehformation",
+                          "Ich hab deine Nase - Scheiße er hat ne Nase",
+                          "Gott ist Brot und wir haben ihn getoastet",
+                          "Schau mein Pferd, mein Pferd ist unglaublich",
                           "Jede Farbe kann besonders gut mit einem Element umgehen",
                           "Wir sind die wahren Spezialisten, unsere Spezialausrüstung ist dort in den Kisten",
-                          "Wir produzieren Fleisch - Fleisch, Fleisch, Fleisch"};
+                          "Wir produzieren Fleisch - Fleisch, Fleisch, Fleisch",
+                          "Wein auf Bier, das rat ich dir. Bier auf Wein ... das rat ich dir"};
 
 
     void Start()
