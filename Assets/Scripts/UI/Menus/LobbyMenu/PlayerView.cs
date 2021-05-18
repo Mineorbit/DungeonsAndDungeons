312653f06dcf46131f99c840da368b8af0abaf3c
mineorbit <mineorbitbeta@gmail.com>
2021-05-18 18:53:26 +0200

    fix

--- Assets/Scripts/UI/Menus/LobbyMenu/PlayerView.cs
+++ Assets/Scripts/UI/Menus/LobbyMenu/PlayerView.cs
@@ -15,6 +15,7 @@ public class PlayerView : MonoBehaviour
         playerElements = new PlayerElement[4];
         for (var i = 0; i < playerElements.Length; i++)
             playerElements[i] = transform.Find(i.ToString()).GetComponent<PlayerElement>();
+        UpdatePlayerView();
     }
 
     public void Update()
@@ -26,6 +27,11 @@ public class PlayerView : MonoBehaviour
     public void UpdatePlayerView()
     {
         for (var i = 0; i < playerElements.Length; i++)
-            playerElements[i].UpdateElement(PlayerManager.playerManager.players[i]);
+        {
+            bool activate = PlayerManager.playerManager.players[i] != null;
+            playerElements[i].gameObject.SetActive(activate);
+            if(activate)
+                playerElements[i].UpdateElement(PlayerManager.playerManager.players[i]);
+        }
     }
 }
