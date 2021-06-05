97481772af2e30e39a8ead37247468bb8ac70e7b
mineorbit <mineorbitbeta@gmail.com>
2021-06-06 00:11:30 +0200

    added upload and login support

--- Assets/Scripts/NetInterface.cs
+++ Assets/Scripts/NetInterface.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NetInterface : MonoBehaviour
 {
     public CanvasGroup canvasGroup;
+    private Button downloadButton;
     void Start()
     {
-        
+        SetupNetInterface();
     }
+
+    void SetupNetInterface()
+    {
+        downloadButton = transform.Find("Download").GetComponent<Button>();
+    }
+    
     public void OnEnable()
     {
         canvasGroup.alpha = 1;
