00df238c4b94310078fd3aead4d95828d3f64bce
mineorbit <mineorbitbeta@gmail.com>
2021-06-18 00:08:14 +0200

    fix to text fonts

--- Assets/Scripts/UI/Menus/UploadMenu.cs
+++ Assets/Scripts/UI/Menus/UploadMenu.cs
@@ -12,13 +12,28 @@ using UnityEngine.UI;
 
 public class UploadMenu : MenuPage
 {
-    public static NetLevel.LevelMetaData levelToUpload;
+   
 
 
-    private TextMeshProUGUI nameText;
-    private Button uploadButton;
+    private static TextMeshProUGUI nameText;
+    private static Button uploadButton;
     
+    public static NetLevel.LevelMetaData _levelToUpload;
+    public static NetLevel.LevelMetaData levelToUpload{
+        get
+        {
+            return _levelToUpload;
+        }
     
+        set
+        {
+            _levelToUpload = value;
+            if (nameText != null)
+            {
+                nameText.text = levelToUpload.FullName;
+            }
+        }
+    }
 
     public FileStructureProfile levels;
     public FileStructureProfile compressedLevels;
@@ -28,6 +43,7 @@ public class UploadMenu : MenuPage
         index = 5;
         var Interface = transform.Find("Interface");
         nameText = Interface.Find("LevelName").GetComponent<TextMeshProUGUI>();
+        Interface.Find("Info").GetComponent<TextMeshProUGUI>().text = "";
         uploadButton = Interface.Find("Upload").GetComponent<Button>();
         uploadButton.onClick.AddListener(
             ()=>HttpManager.StartUpload(levelToUpload));
