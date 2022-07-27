31854f57220103bab7550739ecebb6bea8f16a54
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-07-27 22:54:28 +0200

    added level object data

---
+++ Scripts/LevelObjectData.gd
@@ -0,0 +1,19 @@
+extends Resource
+class_name LevelObjectData
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var name = "Default Level Object"
+export var tiled = false
+export var gridScale = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
