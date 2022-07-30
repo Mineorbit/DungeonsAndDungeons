026248dae13f7a7142c9a14df1b6797970b6e1fa
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-07-30 22:12:35 +0200

    added new 3d view

---
+++ Scripts/Rotation.gd
@@ -0,0 +1,18 @@
+extends MeshInstance
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	rotate(Vector3(0,1, 0), 0.25*PI*delta)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
