f67d3eaaf2edc8dab0d524facbafc53b36162818
mineorbit <mineorbitbeta@gmail.com>
2022-09-11 20:19:57 +0200

    fix again

--- Scripts/LevelObjectList.gd
+++ Scripts/LevelObjectList.gd
@@ -1,14 +1,17 @@
 extends Node3D
 
 
-var display = true
+@export var display = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
 	var i = 0
 	for levelObject in get_children():
-		levelObject.transform.origin = Vector3(4*i,0,0)
+		levelObject.transform.origin = Vector3(2*i + 1,0,0)
 		i = i+1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if display:
+		for child in get_children():
+			child.rotate(Vector3.UP,delta)
