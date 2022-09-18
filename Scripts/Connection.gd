4eb7a533dac2cdcf7f8885896c2c93fbed7d2abc
mineorbit <mineorbitbeta@gmail.com>
2022-09-18 20:06:17 +0200

    more done

---
+++ Scripts/Connection.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+@onready var point_a = $A
+@onready var point_b = $B
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
