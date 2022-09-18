87db2a3d14d13b27c4be9daf5fa58ac1ac98030f
mineorbit <mineorbitbeta@gmail.com>
2022-09-18 21:28:34 +0200

    fixed Connection effect

--- Scripts/Connection.gd
+++ Scripts/Connection.gd
@@ -3,19 +3,27 @@ extends Node3D
 @onready var point_a = $A
 @onready var point_b = $B
 @onready var wire = $Wire
-
+@onready var wiremodel = $Wire/WireModel
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 var dir = Vector3.ZERO
 var dirAB = Vector3.ZERO
+var scaling = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	wire.global_transform.origin = (point_a.global_transform.origin + point_b.global_transform.origin)/2
+	#project targetpoint onto plane of AB direction on centerpoint
+	dirAB = (point_a.global_transform.origin - point_b.global_transform.origin)
+	dirAB = dirAB.normalized()
+	var wirecenter = (point_a.global_transform.origin + point_b.global_transform.origin)/2
+	var targetpoint = Plane(dirAB,0).project(Constants.builderPosition)+wirecenter
+	wire.global_transform.origin = wirecenter
 	
 	dir = Constants.builderPosition-wire.global_transform.origin
 	dir = dir.normalized()
-	dirAB = (point_a.global_transform.origin - point_b.global_transform.origin).normalized()
+	scaling = dirAB.length()
+	dirAB = dirAB.normalized()
 	dir = dir.cross(dirAB)
-	wire.look_at(Constants.builderPosition,dir)
+	wire.look_at(targetpoint,dir)
+	wiremodel.scale = Vector3(scaling*0.9,0.03,0.03)
