e543b89032652468a6529ab1fee5023f7835b882
mineorbit <mineorbitbeta@gmail.com>
2022-09-20 17:00:43 +0200

    fixed controller

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -8,7 +8,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	player.move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	player.move_direction.z = Input.get_action_strength("back") - Input.get_action_strength("forward")
-	player.should_jump = Input.is_action_just_pressed("jump")
-	super._physics_process(delta)
+	if player != null:
+		player.move_direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+		player.move_direction.z = Input.get_action_strength("back") - Input.get_action_strength("forward")
+		player.should_jump = Input.is_action_just_pressed("jump")
+		super._physics_process(delta)
