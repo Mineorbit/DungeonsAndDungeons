53611906b60725e2aa762fb3635dcdb7b2bec246
mineorbit <mineorbitbeta@gmail.com>
2022-10-06 14:13:32 +0200

    fixed stuff

--- Scripts/ServerNetworkManagement.gd
+++ Scripts/ServerNetworkManagement.gd
@@ -4,6 +4,9 @@ extends Node3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var peer = ENetMultiplayerPeer.new()
+	var server_camera = load("res://Prefabs/Camera.tscn").instantiate()
+	add_child(server_camera)
+	server_camera.global_transform.origin = Vector3(0,2,12)
 	#multiplayer.peer_connected.connect(self.create_player)
 	
 	#multiplayer.peer_disconnected.connect(self.destroy_player)
