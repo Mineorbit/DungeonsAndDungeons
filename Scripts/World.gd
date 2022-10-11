c14bb63678d822b8926f9b0dda5a8d9aa85a346d
mineorbit <mineorbitbeta@gmail.com>
2022-10-12 00:02:55 +0200

    fix

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -5,7 +5,7 @@ var level
 
 # Called when the node enters the scene tree for the first time.
 func start(levelpath = null,immediate = false):
-	create_new_level()
+	prepare_level()
 	if levelpath != null:
 		await level.load(levelpath,immediate)
 		
@@ -15,9 +15,13 @@ func start(levelpath = null,immediate = false):
 	print(level.Entities.get_children())
 	
 
-func create_new_level():
+func prepare_level():
 	level = load("res://Prefabs/Level.tscn").instantiate()
 	add_child(level)
+	
+
+func create_new_level():
+	prepare_level()
 	level.setup_new()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
