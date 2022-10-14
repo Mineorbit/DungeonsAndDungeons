9d1a2557b34c5e71035ee18703718c16dfbf9475
mineorbit <mineorbitbeta@gmail.com>
2022-10-14 15:19:28 +0200

    fix

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -5,12 +5,14 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+@onready var ipText: TextEdit = $CanvasLayer/Control/CenterContainer/HBoxContainer/CenterContainer/VBoxContainer/IP
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 
 func open_play_menu():
+	Constants.remoteAddress = ipText.text
 	Bootstrap.start_play()
 
 
