9ff5f8d9d305f6e8cd70cd940e0c7a806f1fe695
mineorbit <mineorbitbeta@gmail.com>
2022-10-21 19:09:26 +0200

    fix

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,18 +4,3 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-@onready var ipText: TextEdit = $CanvasLayer/Control/CenterContainer/HBoxContainer/CenterContainer/VBoxContainer/IP
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-func open_play_menu():
-	Constants.remoteAddress = ipText.text
-	print("'"+str(Constants.remoteAddress)+"'")
-	Bootstrap.start_play()
-
-
-func _open_edit_menu():
-	Bootstrap.start_edit()
---
+++ Scripts/MainMenu/Main.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var ipText: TextEdit = $CenterContainer/HBoxContainer/CenterContainer/VBoxContainer/IP
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+func open_play_menu():
+	Constants.remoteAddress = ipText.text
+	print("'"+str(Constants.remoteAddress)+"'")
+	Bootstrap.start_play()
+
+
+func _open_edit_menu():
+	Bootstrap.start_edit()
