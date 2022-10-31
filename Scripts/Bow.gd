69fb00e3d30a9d6902aa73a94fd3b5eb315e67aa
mineorbit <mineorbitbeta@gmail.com>
2022-10-31 17:43:27 +0100

    fix

---
+++ Scripts/Bow.gd
@@ -0,0 +1,9 @@
+extends ItemEntity
+
+
+func OnAttach(new_owner):
+	super.OnAttach(new_owner)
+
+# eventuell bei boden kontakt oder so eigenen on_entity_melee_strike triggern
+func Use():
+	super.Use()
