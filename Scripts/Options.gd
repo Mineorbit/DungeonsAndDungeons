80990fd9da9ee084886916b133b5b25351bac804
mineorbit <mineorbitbeta@gmail.com>
2022-11-11 16:01:31 +0100

    add title screen

--- Scripts/Options.gd
+++ Scripts/Options.gd
@@ -3,9 +3,10 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready(): 
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	#DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 	# this changes the view mode
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
