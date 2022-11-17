384c7a9852dd8f49312bb8c20752c17bbf62b3cf
mineorbit <mineorbitbeta@gmail.com>
2022-11-17 21:50:21 +0100

    fix

--- Scripts/Blog.gd
+++ Scripts/Blog.gd
@@ -2,12 +2,6 @@ extends Enemy
 
 
 @onready var vision: ShapeCast3D = $VisionCheck
-@onready var animation_player = $BlogModel/AnimationPlayer
-@onready var model_animation_tree: AnimationTree = $AnimationTree
-func _ready():
-	super._ready()
-	model_animation_tree.anim_player = "./BlogModel/AnimationPlayer"
-	model_animation_tree.active = true
 
 
 func _physics_process(delta):
