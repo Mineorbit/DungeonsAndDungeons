61048d0d1bf79d5e2fd9e35622ff532b0d659f04
unknown <maximilian.stillger@arcor.de>
2022-12-10 20:34:26 +0100

    bomb now explodes

--- Scripts/Bomb.gd
+++ Scripts/Bomb.gd
@@ -1,8 +1,13 @@
 extends RigidBody3D
 
+@onready var bombArea = $BombArea
 
 
-	
+func explode():
+	for body in bombArea.get_overlapping_bodies():
+		body.Hit(25,self)
+	despawn()
+
 func despawn():
 	queue_free()
 
