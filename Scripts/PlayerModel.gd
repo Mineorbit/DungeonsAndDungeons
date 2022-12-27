e10372a126c301e7f30c4b9361f221594467ec72
unknown <maximilian.stillger@arcor.de>
2022-12-27 23:00:42 +0100

    added fall animation

--- Scripts/PlayerModel.gd
+++ Scripts/PlayerModel.gd
@@ -9,16 +9,19 @@ func _ready():
 	pass # Replace with function body.
 
 var speed = 0
+var yspeed = 0
 
 func _physics_process(delta):
 	var last_speed_pos = lastpos
 	var speed_pos = global_transform.origin
+	yspeed = speed_pos.y - last_speed_pos.y
 	last_speed_pos.y = 0
 	speed_pos.y = 0
 	speed = (speed_pos - last_speed_pos).length()
 	lastpos = global_transform.origin
-	lastpos
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	anim_tree["parameters/speed/blend_amount"] = speed*8
+	print(max(0,-8*yspeed))
+	anim_tree["parameters/fall/blend_amount"] = max(0,-8*yspeed)
 	
