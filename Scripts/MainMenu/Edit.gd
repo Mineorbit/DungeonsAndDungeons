c64f9edaa478a24c73d43c38daf66c54f8f41896
unknown <maximilian.stillger@arcor.de>
2023-01-08 15:16:19 +0100

    added local level deletion

--- Scripts/MainMenu/Edit.gd
+++ Scripts/MainMenu/Edit.gd
@@ -3,11 +3,16 @@ extends Control
 @onready var subMenu: ScrollContainer = $SubMenu
 @onready var nameText: TextEdit = $SubMenu/GridContainer/ColorRect/MarginContainer/TextEdit
 @onready var level_list = $SubMenu/GridContainer/VSplitContainer/LevelList
-@onready var start_edit_button: Button = $SubMenu/GridContainer/VSplitContainer/AspectRatioContainer3/Edit
+@onready var start_edit_button: Button = $SubMenu/GridContainer/VSplitContainer/AspectRatioContainer3/HBoxContainer/Edit
+@onready var delete_button: Button = $SubMenu/GridContainer/VSplitContainer/AspectRatioContainer3/HBoxContainer/Delete
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	level_list.set_level_list(load_level_list())
-	level_list.on_selection.connect(func (x): start_edit_button.modulate = Color.WHITE)
+	level_list.on_selection.connect(func (x): 
+		start_edit_button.modulate = Color.WHITE
+		delete_button.modulate = Color.WHITE
+		)
 
 
 func load_level_list():
@@ -37,6 +42,16 @@ func start_edit_level():
 		return
 	print("Starting to edit Level: "+str(level_list.selected_level))
 	get_parent().get_parent().get_parent().start_edit(level_list.selected_level)
+
+func _on_delete_level():
+	for file in DirAccess.open("user://level/localLevels/"+str(level_list.selected_level)).get_files():
+			DirAccess.remove_absolute("user://level/localLevels/"+str(level_list.selected_level)+"/"+str(file))
+	for file in DirAccess.open("user://level/localLevels/"+str(level_list.selected_level)+"/chunks").get_files():
+			DirAccess.remove_absolute("user://level/localLevels/"+str(level_list.selected_level)+"/chunks/"+str(file))
+	
+	DirAccess.remove_absolute("user://level/localLevels/"+str(level_list.selected_level)+"/chunks")
+	DirAccess.remove_absolute("user://level/localLevels/"+str(level_list.selected_level))
+	level_list.set_level_list(load_level_list())
 
 func _on_edit_level():
 	selectedSubmenu = 0
