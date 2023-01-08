70c7b12cc3a3b3134d522a4ab5430cd93a879f7f
unknown <maximilian.stillger@arcor.de>
2023-01-08 16:33:38 +0100

    improved

--- Scripts/MainMenu/Edit.gd
+++ Scripts/MainMenu/Edit.gd
@@ -1,18 +1,22 @@
 extends Control
 
-@onready var subMenu: ScrollContainer = $SubMenu
-@onready var nameText: TextEdit = $SubMenu/GridContainer/ColorRect/MarginContainer/TextEdit
-@onready var level_list = $SubMenu/GridContainer/VSplitContainer/LevelList
-@onready var start_edit_button: Button = $SubMenu/GridContainer/VSplitContainer/AspectRatioContainer3/HBoxContainer/Edit
-@onready var delete_button: Button = $SubMenu/GridContainer/VSplitContainer/AspectRatioContainer3/HBoxContainer/Delete
+@onready var nameText: TextEdit = $TabContainer/NewLevel/VBoxContainer/MarginContainer/LevelName
+@onready var level_list = $TabContainer/EditLevel/LevelList
+@onready var start_edit_button: Button = $TabContainer/EditLevel/CenterContainer/HBoxContainer/Edit
+@onready var delete_button: Button = $TabContainer/EditLevel/CenterContainer/HBoxContainer/Delete
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	level_list.set_level_list(load_level_list())
+	var levels = load_level_list()
+	level_list.set_level_list(levels)
 	level_list.on_selection.connect(func (x): 
+		pass
 		start_edit_button.modulate = Color.WHITE
 		delete_button.modulate = Color.WHITE
 		)
+	if levels.size() == 0:
+		pass
+		#open new level tab immediately
 
 
 func load_level_list():
@@ -22,20 +26,12 @@ func load_level_list():
 		levels.append({"name":l,"ulid":l})
 	return levels
 
-@export var selectedSubmenu = 0
 
-func _process(delta):
-	#the plus 32 is because of the scroll bar
-	subMenu.scroll_horizontal = selectedSubmenu*(size.x+32) * 0.2 + subMenu.scroll_horizontal * 0.8
-	#subMenu.scroll_horizontal = selectedSubmenu*(628)
-
-func _on_new_level():
-	selectedSubmenu = 1
 
 func start_new_level():
 	get_parent().get_parent().get_parent().start_edit()
-	Constants.currentLevel.level_name = nameText.text
-	Constants.currentLevel.save()
+	Constants.World.level.level_name = nameText.text
+	Constants.World.level.save()
 
 func start_edit_level():
 	if level_list.selected_level == null:
@@ -52,6 +48,3 @@ func _on_delete_level():
 	DirAccess.remove_absolute("user://level/localLevels/"+str(level_list.selected_level)+"/chunks")
 	DirAccess.remove_absolute("user://level/localLevels/"+str(level_list.selected_level))
 	level_list.set_level_list(load_level_list())
-
-func _on_edit_level():
-	selectedSubmenu = 0
--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -37,4 +37,5 @@ func exit_to_main_menu():
 	unpause()
 	Bootstrap.start_main_menu()
 	multiplayer.set_multiplayer_peer(null)
-	Signals.on_new_world_created.disconnect(Constants.entity_control_function)
+	if Constants.entity_control_function != null:
+		Signals.on_new_world_created.disconnect(Constants.entity_control_function)
