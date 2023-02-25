5c864c5a5dc65e465afb0944bcdc9433e1d0cc15
unknown <maximilian.stillger@arcor.de>
2023-02-25 21:26:26 +0100

    button collision is now correct

---
+++ Scripts/SwitchCollision.gd
@@ -0,0 +1,8 @@
+extends AnimatableBody3D
+
+var skeleton: Skeleton3D
+
+func _ready():
+	skeleton = get_parent().get_parent()
+func _physics_process(delta):
+	transform = skeleton.get_bone_global_pose(0)
