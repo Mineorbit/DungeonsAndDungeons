717c20e4183d0319fe71cd8d41bcd93f6ee9e724
unknown <maximilian.stillger@arcor.de>
2023-04-02 13:15:26 +0200

    fixed shield collision

--- Scripts/Level/LevelObject/Entities/Items/Shield.gd
+++ Scripts/Level/LevelObject/Entities/Items/Shield.gd
@@ -19,10 +19,10 @@ func OnDettach():
 # eventuell bei boden kontakt oder so eigenen on_entity_melee_strike triggern
 
 func damage_modifier(direction):
-	var hit_direction = direction.normalized()
-	print(itemOwner.global_transform.basis.z)
-	print(hit_direction)
-	if in_use and hit_direction.dot(itemOwner.global_transform.basis.z) > 0:
+	var hit_direction = -direction.normalized()
+	var forward_direction = itemOwner.global_transform.basis.x
+	var relation = hit_direction.dot(forward_direction)
+	if in_use and relation > 0:
 		return 0
 	return 1 
 
