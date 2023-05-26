92eef58c9faebbdbea39ba83d4dbfb07d170b3ba
unknown <maximilian.stillger@arcor.de>
2023-05-26 04:44:09 +0200

    fix

---
+++ Scripts/Level/LevelObject/Entities/Key.gd
@@ -0,0 +1,11 @@
+extends ItemEntity
+class_name Key
+
+
+@onready var keyholeholder: Area3D = $KeyholeDetector
+
+func Use():
+	super.Use()
+	if keyholeholder.has_overlapping_bodies():
+		var locked_object = keyholeholder.get_overlapping_bodies()[0].get_parent()
+		locked_object.open()
---
+++ Scripts/Level/LevelObject/LockedDoor.gd
@@ -0,0 +1,53 @@
+extends Node3D
+
+
+@onready var doorModel: Node3D = $LockedDoorModel
+@onready var nav = $Navigation
+@onready var collision = $Collision
+
+@export var is_open = false:
+	set(val):
+		if val:
+			if doorModel != null:
+				doorModel.hide()
+				#doorModel.switch()
+		else:
+			if doorModel != null:
+				doorModel.show()
+				#doorModel.switchback()
+		is_open = val
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	close()
+
+
+func start():
+	close()
+
+func reset():
+	close()
+
+
+func open():
+	add_child(nav)
+	if collision.get_parent() != null:
+		remove_child(collision)
+	is_open = true
+	
+
+func close():
+	remove_child(nav)
+	if collision.get_parent() == null:
+		add_child(collision)
+	is_open = false
+	
+
+
+func prepare_for_navmesh_build():
+	print("Disabled before baking Nav Mesh")
+	#open()
+	
+func restore_after_navmesh_build():
+	print("Enabling before baking Nav Mesh")
+	#close()
